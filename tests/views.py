48a5431fcbad761ba7b5767a21bd101f30889159
NINJABuoy42 <78472580+NINJABuoy42@users.noreply.github.com>
2021-10-04 15:38:22 +0530

    Update views.py

--- tests/views.py
+++ tests/views.py
@@ -9,6 +9,5 @@ def home(request):
 def yearbook(request):
     sheet_id='1SQTDlT49uR_aShPF2J96tm0D83u8KAfdqAW1a6vnJ2o'
     df=pd.read_csv(f'https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv')
-    record=df.to_dict(orient='records')
-    print(record)
+    record=df.to_dict(orient='record')
     return render(request,'yearbook.html',{'record':record})
