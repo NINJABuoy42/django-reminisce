46b6ebde7f7aa0f5aa671fc69850e5402013b63d
NINJABuoy42 <78472580+NINJABuoy42@users.noreply.github.com>
2021-10-05 16:17:20 +0530

    Update views.py

--- tests/views.py
+++ tests/views.py
@@ -11,3 +11,11 @@ def yearbook(request):
     df=pd.read_csv(f'https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv')
     record=df.to_dict(orient='record')
     return render(request,'yearbook.html',{'record':record})
+
+
+def profile(request):
+    sheet_id='1SQTDlT49uR_aShPF2J96tm0D83u8KAfdqAW1a6vnJ2o'
+    df=pd.read_csv(f'https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv')
+    profile=df.to_dict(orient='profile')
+    #print(df)
+    return render(request,'profile.html',{'profile':profile})
